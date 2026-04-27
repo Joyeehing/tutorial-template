b86285a1d2b5baec86324e4177b6bdd5e7c8241a
Joyeehing <hingsiyun@gmail.com>
2026-04-27 16:40:56 +0100

    Update project information in conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Thryft'
+copyright = '2026, SETAP4C'
+author = 'SETAP4C'
 
 release = '0.1'
 version = '0.1.0'
